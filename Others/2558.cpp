ff1959e1077c58ea89029ac4bd0fc2f9c277d424
azzyjk <56161518+azzyjk@users.noreply.github.com>
2022-02-22 19:28:38 +0900

    Class 외 문제 풀이

---
+++ Others/2558.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
+    return 0;
+}
