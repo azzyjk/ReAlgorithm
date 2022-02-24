e8a75033a020fe98dff1fb47d487f7982f03ebed
azzyjk <56161518+azzyjk@users.noreply.github.com>
2022-02-24 21:53:50 +0900

    Class 외 문제 풀이

---
+++ Others/5337.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << ".  .   .\n";
+    cout << "|  | _ | _. _ ._ _  _\n";
+    cout << "|/\\|\(/.|\(_.\(_)\[ | )\(/.\n";
+    return 0;
+}
